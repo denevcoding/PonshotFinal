60744031203d25a11535a513436729987a4c9ee0
DenevCode <denevdesign@gmail.com>
2024-03-16 12:39:45 -0500

    Before input Manager implementation

---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputSystem.onAnyButtonPress
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
